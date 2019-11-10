13d68cbd20cf314905076610e49f37b18be12ac9
amdevg <jeberry308@gmail.com>
2019-11-09 18:10:32 -0600

    Added GCloud to Functions; Error on implicit import; Error on .onChange - NOT WORK ON BUCKET BUILDER?; Switching to Upload Functionality;

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,30 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+
+// tslint:disable-next-line: no-implicit-dependencies
+// import * as vision from '@google-cloud/vision';
+// const visionClient = new vision.ImageAnotatorClient();
+
+// const bucketName = 'gs://buseinesscardreader.appspot.com';
+
+// export const imageTagger = functions.storage.bucket(bucketName).onChange( async event => {
+//         const object = event.data;
+//         const filePath = object.name;
+//         const imageUri = 'gs://${bucketName}/${filePath}';
+
+//         //const docId = filePath.split('.jpg')[0];
+//         const docRef  = admin.firestore().collection('businessCards').doc('CardTest1');
+
+//         const textRequest = await visionClient.documentTextDetection(imageUri)
+//         const fullText = textRequest[0].textAnnotations[0]
+//         const text =  fullText ? fullText.description : null
+
+//         const data = { text };
+
+//         return docRef.set(data);
+//     });
