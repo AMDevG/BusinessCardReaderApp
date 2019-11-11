4084594e14bebd51b03546d16b5fe57ffcbab80e
amdevg <jeberry308@gmail.com>
2019-11-11 00:57:19 -0600

    BusinessCardService generating uuid, email: undefined, +2; BUG: ** Refresh/Submit/NavigationAway GUARDS PATH - REDIRECTS TO LOGIN

--- src/app/business-card.ts
+++ src/app/business-card.ts
@@ -1,4 +1,7 @@
+import { uuid } from '../app/util/uuid';
+
 export class BusinessCard {
+    userId: string;
     firstName: string;
     lastName: string;
     companyName: string;
@@ -7,6 +10,7 @@ export class BusinessCard {
 
     constructor(firstName: string, lastName: string, companyName: string, email: string,
                 phone: string ) {
+                    this.userId = uuid();
                     this.firstName = firstName;
                     this.lastName = lastName;
                     this.companyName = companyName;
--- src/app/upload.service.ts
+++ src/app/upload.service.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase/app';
 import { BusinessCard } from './business-card';
+import { BusinessCardService } from './business-card.service';
 
 @Injectable({
   providedIn: 'root'
@@ -17,11 +18,16 @@ export class UploadService {
   constructor(private authService: AuthService, private fireAuthService: AngularFireAuth) { }
 
 uploadToStorage(businessCard: BusinessCard) {
-  // const storageRef = firebase.storage().ref();
-  // console.log('Storage ref is ${storageRef}');
-  console.log('Uplading bcard: ', businessCard.companyName);
+  // const API_ENDPIOINT = 'https://buseinesscardreader-4e06a-carddb.firebaseio.com/';
+  // const ref = firebase.storage().ref();
+  // console.log('Storage ref is ${ref}');
+  console.log('Uplading bcard ID: ', businessCard.userId);
+  // var imgUri = '[IMGURI]';
+  // ref.putString('id', 'base64').then(function(snapshot) {
+  console.log('Uploaded a Full Card minus ID', businessCard);
+}
 
   // NEED TO CALL FIREDATABASE UPLOAD TO COLLECTIONS; ATTACHED TO USERID & ADD IMGURI + BASE64; 
   // RETURN SUCCESS; NO IMG CARD ON NGIF, CLEAR SCREAN OF CAPTURE;
 }
-}
+
