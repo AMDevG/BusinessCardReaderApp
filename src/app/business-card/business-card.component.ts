8088f1b49d10a731ac8c75c2498f6bdbedf93f10
amdevg <jeberry308@gmail.com>
2019-11-12 20:44:54 -0600

    Moved FileUpload Component to BusinessCard;

--- src/app/business-card/business-card.component.ts
+++ src/app/business-card/business-card.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BusinessCardService } from '../business-card.service';
+import { VisionService } from '../vision.service';
+import { UploadService } from '../upload.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-business-card',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusinessCardComponent implements OnInit {
 
-  constructor() { }
+  base64Img: string;
+  filePathUri: string;
+  userId: string;
+  uploadForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) { this.createForm(); }
+
+  createForm() {
+    this.uploadForm = this.fb.group({
+      firstNameInput:  ['', [Validators.required, Validators.minLength(3)]],
+      lastNameInput:  ['', [Validators.required, Validators.minLength(3)]],
+      companyInput: ['', [Validators.required, Validators.minLength(3)]],
+      emailInput:  ['', [Validators.required, Validators.email]],
+      phoneInput: ['', [Validators.required, Validators.minLength(3)]]
+    });
+    // return FormGroup;
   }
+  ngOnInit() {}
+
+  // onSubmit(value: any) {
+  //   // PASSES BUSINESSCARD OBJECT TO UPLOADSERVICE TO HANDE DB WRITES/HTTP; WITH UUID FROM BCARD
+  //   // SERVICE
+  //   let submittedBCard = this.businessCardService
+  //         .createNewBusinessCard(value.firstName, value.lastNameInput,
+  //                               value.companyInput, value.email, value.phoneInput);
+  //   console.log('Calling vision Service');
+  //   this.visionService.extractText();
+  //   console.log(this.visionService.getDescriptionArray());
+  // }
+
+
 
 }
--- src/app/upload.service.ts
+++ src/app/upload.service.ts
@@ -4,6 +4,7 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase/app';
 import { BusinessCard } from './business-card';
 import { BusinessCardService } from './business-card.service';
+import { FormBuilder, FormGroup, Validators, Form } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -12,19 +13,28 @@ export class UploadService {
   base64Img: string;
   filePathUri: string;
   userId: string;
-  // uploadPath: string;
-  // fileName: string;
+  uploadForm: FormGroup;
 
-  constructor(private authService: AuthService, private fireAuthService: AngularFireAuth) { }
+  constructor(private authService: AuthService, private fireAuthService: AngularFireAuth, private fb: FormBuilder) { }
 
+  createForm() {
+    this.uploadForm = this.fb.group({
+      firstNameInput:  ['', [Validators.required, Validators.minLength(3)]],
+      lastNameInput:  ['', [Validators.required, Validators.minLength(3)]],
+      companyInput: ['', [Validators.required, Validators.minLength(3)]],
+      emailInput:  ['', [Validators.required, Validators.email]],
+      phoneInput: ['', [Validators.required, Validators.minLength(3)]]
+    });
+    return FormGroup;
+  }
 uploadToStorage(businessCard: BusinessCard) {
   // const API_ENDPIOINT = 'https://buseinesscardreader-4e06a-carddb.firebaseio.com/';
   // const ref = firebase.storage().ref();
   // console.log('Storage ref is ${ref}');
-  console.log('Uplading bcard ID: ', businessCard.userId);
+  // console.log('Uplading bcard ID: ', businessCard.userId);
   // var imgUri = '[IMGURI]';
   // ref.putString('id', 'base64').then(function(snapshot) {
-  console.log('Uploaded a Full Card minus ID', businessCard);
+  // console.log('Uploaded a Full Card minus ID', businessCard);
 }
 
   // NEED TO CALL FIREDATABASE UPLOAD TO COLLECTIONS; ATTACHED TO USERID & ADD IMGURI + BASE64; 
