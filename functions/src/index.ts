cb15938b61039b29e683a633b696d1da319e5eac
amdevg <jeberry308@gmail.com>
2019-11-16 04:14:31 -0600

    reinit FB

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,69 +1,8 @@
+import * as functions from 'firebase-functions';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-
-// ONCHANGE NO LONGER SUPPORTED SEE DOCS LINK BELOW FOR UPDATED FIREBASE API
-//  https://firebase.google.com/docs/functions/beta-v1-diff#storage
-// https://firebase.google.com/docs/functions/get-started
-// https://github.com/firebase/friendlypix
-// detectLandmarksGCS(bucketName, fileName) 
-
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const vision = require('@google-cloud/vision');
-admin.initializeApp();
-const bucketName = 'gs://buseinesscardreader.appspot.com';
-// const testFilePath = 'jberry/Desktop/testCard1.png'
-// [START vision_text_detection]
-export const detectBCard = functions.https.onRequest(async (request: any, response: any) => {
-    const client = new vision.ImageAnnotatorClient();
-    console.log("Hey were in Detect BCard!");
-    const testFileName = 'testCard1.png';
-    console.log("Going to analyze file: ", testFileName);
-    // Performs text detection on the local file
-    // console.log("About to call local text Detection!")
-    // const [result] = await client.textDetection(testFilePath);
-    
-    // Text Detection on Cloud Bucket
-    console.log("About to call cloud text Detection!")
-    const [result] = await client.textDetection(
-    `gs://${bucketName}/${testFileName}`
-    );
-    console.log("Retrueved results");
-    const detections = result.textAnnotations;
-    console.log('Text:');
-    // detections.forEach((text: string) => console.log(text));
-    console.log(detections);
-  });
-
-
-// TUTORIAL CODE FROM https://angularfirebase.com/lessons/google-cloud-vision-with-ionic-and-firebase/
-// export const imageTagger = functions.storage.bucket(bucketName)
-//     .onChange( async event => {
-//         const object = event.data;
-//         const filePath = object.name;
-//         const imageUri = 'gs://${bucketName}/${filePath}';
-
-//         // const docId = filePath.split('.jpg')[0];
-//         const docId = 'CardTest1';
-//         const docRef  = admin.firestore().collection('businessCards').doc(docId);
-
-//         const textRequest = await visionClient.documentTextDetection(imageUri)
-//         const fullText = textRequest[0].textAnnotations[0]
-//         const text =  fullText ? fullText.description : null
-
-//         const data = { text };
-
-//         return docRef.set(data);
-//     });
-
-// TEST CODE FOR ON UPLOAD ========
-//  exports.addMessage = functions.https.onRequest(async (request: any, response: any) => {
-//     // Grab the text parameter.
-//     const original = request.query.text;
-//     // Push the new message into the Realtime Database using the Firebase Admin SDK.
-//     const snapshot = await admin.database().ref('/messages').push({original: original});
-//     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-//     response.redirect(303, snapshot.ref.toString());
-//   }); 
-  //=============
