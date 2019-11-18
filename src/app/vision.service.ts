1050db8f1901ad46111f4d840e1cb85c5e8375a2
amdevg <jeberry308@gmail.com>
2019-11-17 18:19:36 -0600

    Making request with base64; 404 error back from Vision;

--- src/app/vision.service.ts
+++ src/app/vision.service.ts
@@ -6,58 +6,39 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class VisionService {
+  constructor(private httpClient: HttpClient) {}
+
   textArray = [];
   API_URL: string;
   base64Enc: string;
 
   url = 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBZFB9-CMuMag5APKDt_WxZ_BNS1HO-en4';
-  constructor(private httpClient: HttpClient) {
-    // this.API_URL = 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBZFB9-CMuMag5APKDt_WxZ_BNS1HO-en4';
-  }
+
+request = {
+    image: {
+      content: this.base64Enc
+    },
+    features: [ {
+      type: 'TEXT_DETECTION',
+      maxResults: 1,
+    }]
+  };
 
   createRequest(base64) {
     this.base64Enc = base64;
-    console.log('set base64 Enc to: ', this.base64Enc);
+    //this.request['requests'][0]['image']['content'] = this.base64Enc;
+    // console.log('set Conten Enc to: ', this.base64Enc);
+    console.log('Calling text extract');
+    this.extractText();
   }
 
-  request = {
-    requests: [
-    {
-    image: {
-      source: {
-      imageUri:
-       'https://firebasestorage.googleapis.com/v0/b/buseinesscardreader.appspot.com/o/testCard1.jpg?alt=media&token=cdf9a199-9a83-4e4b-9f36-52e3b0c3e0d4',
-      },
-    },
-    features: [
-      {type: 'TEXT_DETECTION',
-      maxResults: 1, }]
-      }]
-    };
-
-
-  // createRequest(baseIn) {
-  //   this.request = {
-  //     requests: [
-  //     {
-  //     image: {
-  //       content: `${baseIn}`
-  //     },
-  //     features: [
-  //       {type: 'TEXT_DETECTION',
-  //       maxResults: 1, }]
-  //       }]
-  //     };
-  //   // this.extractText();
-  //   // console.log(`Base included: ${baseIn}`);
-  //   }
 
     extractText() {
-      // console.log(`Beginning text extraction, req: ${this.request['requests'][0]['image']['content']}`);
-
+      // console.log(`Beginning text extraction, base64: ${this.request['requests'][0]['image']['content']}`);
+      console.log('Calling vision API');
       this.httpClient.post(this.url, this.request)
       .subscribe( (results: any) => {
-        let tmpArr = results.responses[0].textAnnotations;
+        const tmpArr = results.responses[0].textAnnotations;
         console.log('received tmpArr: ', tmpArr);
         tmpArr.forEach(element => {
           this.textArray.push(element['description']);
@@ -78,6 +59,23 @@ export class VisionService {
   getDescriptionArray() {
     return this.textArray;
   }
+
+
+  // createRequest(baseIn) {
+  //   this.request = {
+  //     requests: [
+  //     {
+  //     image: {
+  //       content: `${baseIn}`
+  //     },
+  //     features: [
+  //       {type: 'TEXT_DETECTION',
+  //       maxResults: 1, }]
+  //       }]
+  //     };
+  //   // this.extractText();
+  //   // console.log(`Base included: ${baseIn}`);
+  //   }
 
   // uploadImageToStorage() {
   //     console.log("Mock call to upload Image!");
