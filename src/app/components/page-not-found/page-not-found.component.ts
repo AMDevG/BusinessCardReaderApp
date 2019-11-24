91f13a28a88dd28431a0655dd617140118e1e60c
amdevg <jeberry308@gmail.com>
2019-11-24 00:35:41 -0600

    AuthGuard Working Properly; Back to BCARD Form;

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
+@Component ({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.css']
